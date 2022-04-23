d51b1d1332e93eb91e04542053f826d237001c10
rodrigonevest <56009318+rodrigonevest@users.noreply.github.com>
2022-04-23 11:45:28 -0300

    upload_dataset

--- tslabel_weak/test.py
+++ tslabel_weak/test.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from _tslabel_weak import tslabel_weak as ts
 
-df = pd.read_excel('C:\cursospython\graficos\Cepea_milho.xls')
+df = pd.read_excel('test_dataset\Cepea_milho.xls')
 df.set_index('date', inplace=True)
 
-df_tx = pd.read_excel('C:\cursospython\graficos\Scraping_Cepea_En_Noticias.xlsx', index_col='Date')
+df_tx = pd.read_excel('test_dataset\Scraping_Cepea_En_Noticias.xlsx', index_col='Date')
 df_tx.index = pd.to_datetime(df_tx.index)
 
 test = ts(txt=df_tx, col_txt='Text_Headline',serie=df, pct=0.03, col='real')
